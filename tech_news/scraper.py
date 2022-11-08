6cb36b73f2e9731a954e1761b5683b06fe2d98e2
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-08 16:57:45 -0300

    feat: Cria o fetch que retorna o html do site

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui :)"""
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"user-agent": "Fake user-agent"}
+        )
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+    except (requests.ReadTimeout, requests.HTTPError):
+        return None
 
 
 # Requisito 2
