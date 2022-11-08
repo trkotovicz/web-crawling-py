1996cf9e3ca7c2d2df9c662f64ad03ef4d515760
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-08 18:38:28 -0300

    feat: Cria scrape que retorna o link da próxima página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page = selector.css("a.next.page-numbers::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
