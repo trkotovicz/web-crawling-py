0ace16084429a131fac76c346d426953e14fd866
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-08 22:24:17 -0300

    feat: Cria scrape que retorna informações de uma página de notícia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,29 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("li.meta-author > .author a::text").get()
+    join = ""
+    summary = join.join(
+        selector.css(".entry-content > p:nth-of-type(1) *::text").getall()
+    ).strip()
+    category = selector.css(".entry-header-inner span.label::text").get()
+    comments_count = len(selector.css(".comment").getall())
+    tags = selector.css("a[rel=tag]::text").getall()
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
