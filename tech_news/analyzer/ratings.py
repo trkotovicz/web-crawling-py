fe1f62f429c6f406da37ae8d7b1a8b53b69ff923
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-09 23:44:36 -0300

    feat: Cria função que lista as notícias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_from_db = find_news()
+    sort_news = sorted(
+        news_from_db, key=lambda row: row["comments_count"], reverse=True
+    )
+
+    sorted_news = [
+        (each_new["title"], each_new["url"]) for each_new in sort_news
+    ]
+    print(sorted_news)
+    return sorted_news[:5]
 
 
 # Requisito 11
