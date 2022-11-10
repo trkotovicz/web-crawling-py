608d4aba93a2aa37d6dd82f5bf07de5d4be5ee1e
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-11-09 21:34:48 -0300

    feat: Cria busca no db pelo título

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    """for book in db.books.find({"title": {"$regex": "t"}}):
+        print(book["title"])"""
+    search_in_db = search_news({"title": {"$regex": title, "$options": "i"}})
+    news_found = []
+    for each in search_in_db:
+        news_found.append((each["title"], each["url"]))
+    # print((search_in_db[0]["title"], search_in_db[0]["url"]))
+    print(news_found)
+    return news_found
 
 
 # Requisito 7
